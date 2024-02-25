581915ca281d923b973ca1c025b57e579a837894
Johann Tienhaara <jtienhaara@yahoo.com>
2024-02-25 17:14:55 -0400

    Look for Pods that are not ready, too, in "get_unrunning_pods.sh"

--- kubernetes/get_unrunning_pods.sh
+++ kubernetes/get_unrunning_pods.sh
@@ -9,6 +9,15 @@ kubectl get pods \
            $4 != "Running" && $4 != "Completed" {
                print $0;
            }
+           $4 == "Running" {
+               actual_ready_containers = $3;
+               expected_ready_containers = $3;
+               gsub(/\/[0-9]*$/, "", actual_ready_containers)
+               gsub(/^[0-9]*\//, "", expected_ready_containers)
+               if (actual_ready_containers != expected_ready_containers) {
+                   print $0;
+               }
+           }
           ' \
               || exit 1
 
